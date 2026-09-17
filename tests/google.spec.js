0fd84cbea2272456e707b25de28096b48b2c8ba6
Мари55555555-ghj <marina.stolyarova.2015@mail.ru>
2026-09-18 01:06:15 +0300

    first commit

---
+++ tests/google.spec.js
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('Поиск в Google через Playwright', async ({ page }) => {
+  await page.goto('https://google.com');
+  await page.locator('textarea[name="q"]').fill('QA.GURU Playwright');
+  await page.keyboard.press('Enter');
+  await expect(page).toHaveTitle(/QA.GURU/);
+});
