21d9968ac36da9758384d1ee4718f0cdc4e27518
Мари55555555-ghj <marina.stolyarova.2015@mail.ru>
2026-09-20 15:57:40 +0300

    chore: skip google test

--- tests/google.spec.js
+++ tests/google.spec.js
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('Поиск в Google через Playwright', async ({ page }) => {
+// Добавляем .skip, чбобы Playwright пропускал шаг 
+test.skip ('Поиск в Google через Playwright', async ({ page }) => {
   await page.goto('https://google.com');
   await page.locator('textarea[name="q"]').fill('QA.GURU Playwright');
   await page.keyboard.press('Enter');
