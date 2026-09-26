8626b033a1869562fe03e90c1ee657047d96ab34
Мари55555555-ghj <marina.stolyarova.2015@mail.ru>
2026-09-26 20:20:59 +0300

    feat: настроена генерация русских данных и пароля через faker

--- tests/guru.spec.js
+++ tests/guru.spec.js
@@ -1,24 +1,27 @@
 import { test, expect } from '@playwright/test';
-import { faker } from '@faker-js/faker'; // Подключаем генератор данных
+// Импортуем русскую версию Faker
+import { fakerRU as faker } from '@faker-js/faker'; 
 
 test('test', async ({ page }) => {
-  // Эти строки создают новые случайные данные ПРИ КАЖДОМ запуске теста:
+  // Генерируем полностью случайные имя, email и пароль
   const randomName = faker.person.fullName(); 
   const randomEmail = faker.internet.email();
+  const randomPassword = faker.internet.password({ length: 10 });
 
-  await page.goto('https://realworld.qa.guru');
+await page.goto('https://qa.guru');
   await page.getByRole('link', { name: 'Sign up' }).click();
   
   await page.getByRole('textbox', { name: 'Your Name' }).click();
-  await page.getByRole('textbox', { name: 'Your Name' }).fill(randomName); // Новое имя
+  await page.getByRole('textbox', { name: 'Your Name' }).fill(randomName); 
   
   await page.getByRole('textbox', { name: 'Email' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).fill(randomEmail); // Новая почта
+  await page.getByRole('textbox', { name: 'Email' }).fill(randomEmail); 
   
   await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('876543qw');
+  await page.getByRole('textbox', { name: 'Password' }).fill(randomPassword); 
+  
   await page.getByRole('button', { name: 'Sign up' }).click();
   
-  // Проверяем аватар именно с тем именем, которое сгенерировал Faker:
+  // Проверяем аватар по сгенерированному русскому имени
   await expect(page.getByRole('img', { name: randomName })).toBeVisible();
 });
