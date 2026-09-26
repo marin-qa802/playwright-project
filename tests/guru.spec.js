45e74a7edb783cfbe9df823afe6a8183e9ac06d8
Мари55555555-ghj <marina.stolyarova.2015@mail.ru>
2026-09-26 19:55:02 +0300

    feat: добавлен faker и исправлены тесты

--- tests/guru.spec.js
+++ tests/guru.spec.js
@@ -1,14 +1,24 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker'; // Подключаем генератор данных
 
 test('test', async ({ page }) => {
-  await page.goto('https://realworld.qa.guru/');
+  // Эти строки создают новые случайные данные ПРИ КАЖДОМ запуске теста:
+  const randomName = faker.person.fullName(); 
+  const randomEmail = faker.internet.email();
+
+  await page.goto('https://realworld.qa.guru');
   await page.getByRole('link', { name: 'Sign up' }).click();
+  
   await page.getByRole('textbox', { name: 'Your Name' }).click();
-  await page.getByRole('textbox', { name: 'Your Name' }).fill('yli');
+  await page.getByRole('textbox', { name: 'Your Name' }).fill(randomName); // Новое имя
+  
   await page.getByRole('textbox', { name: 'Email' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).fill('yli@com.ru');
+  await page.getByRole('textbox', { name: 'Email' }).fill(randomEmail); // Новая почта
+  
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('876543qw');
   await page.getByRole('button', { name: 'Sign up' }).click();
-  await page.getByRole('img', { name: 'yli' }).click();
+  
+  // Проверяем аватар именно с тем именем, которое сгенерировал Faker:
+  await expect(page.getByRole('img', { name: randomName })).toBeVisible();
 });
